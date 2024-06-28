805859c4f02730f53aea87ac7e8f781456654300
OlgaPorhun <porhunolga@gmail.com>
2024-06-28 22:18:54 +0300

    goit-react-hw-02 (add)

---
+++ src/App.jsx
@@ -0,0 +1,61 @@
+import { useState, useEffect } from "react";
+import Description from "./components/Description/Description";
+import Options from "./components/Options/Options";
+import Feedback from "./components/Feedback/Feedback";
+import Notification from "./components/Notification/Notification";
+import styles from "./App.module.css";
+
+const App = () => {
+  const [feedback, setFeedback] = useState({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  });
+
+  useEffect(() => {
+    const storedFeedback = JSON.parse(localStorage.getItem("feedback"));
+    if (storedFeedback) {
+      setFeedback(storedFeedback);
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("feedback", JSON.stringify(feedback));
+  }, [feedback]);
+
+  const updateFeedback = (feedbackType) => {
+    if (feedbackType === "reset") {
+      setFeedback({ good: 0, neutral: 0, bad: 0 });
+    } else {
+      setFeedback((prevFeedback) => ({
+        ...prevFeedback,
+        [feedbackType]: prevFeedback[feedbackType] + 1,
+      }));
+    }
+  };
+
+  const totalFeedback = feedback.good + feedback.neutral + feedback.bad;
+  const positivePercentage = totalFeedback
+    ? Math.round((feedback.good / totalFeedback) * 100)
+    : 0;
+
+  return (
+    <div className={styles.app}>
+      <Description />
+      <Options updateFeedback={updateFeedback} totalFeedback={totalFeedback} />
+      {totalFeedback > 0 ? (
+        <Feedback
+          good={feedback.good}
+          neutral={feedback.neutral}
+          bad={feedback.bad}
+          totalFeedback={totalFeedback}
+          positivePercentage={positivePercentage}
+        />
+      ) : (
+        <Notification />
+      )}
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Notification/Notification.jsx
@@ -0,0 +1,9 @@
+import styles from "./Notification.module.css";
+
+const Notification = () => (
+  <div className={styles.notification}>
+    <p>No feedback given</p>
+  </div>
+);
+
+export default Notification;
